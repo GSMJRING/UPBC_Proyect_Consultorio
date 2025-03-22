60efe65ea6f3b2c13511ccf98fcbb66f1ed64e33
GSMJRING <gsmjring@gmail.com>
2025-03-21 18:56:20 -0700

    Modificacion de Interfaz_Medico

--- Interfaz_Medico.py
+++ Interfaz_Medico.py
@@ -42,46 +42,58 @@ class Window(QMainWindow):
         self.lPaciente.setGeometry(16, 160, 96, 29)
         self.lPaciente.setFont(QFont('Segoe UI', 14, QFont.Weight.Bold))
         self.lPaciente.setText('Paciente :')
+        self.lHistorialClinico = QLabel(self)
+        self.lHistorialClinico.setGeometry(928, 24, 208, 34)
+        self.lHistorialClinico.setFont(QFont('Segoe UI', 16, QFont.Weight.Bold))
+        self.lHistorialClinico.setText('Historial Clinico')
+        
+        self.lineEdit1 = QLineEdit(self.GralData)
+        self.lineEdit1.setGeometry(120, 160, 345, 33)
+        self.lineEdit1.setFont(QFont('Segoe UI', 9))
+
+        # Tablas de diagnostico y Entrada de texto
         self.PlainDiagnostico = QPlainTextEdit(self.GpDiagnostico)
         self.PlainDiagnostico.setGeometry(16, 32, 608, 256)
         self.PlainDiagnostico.setFont(QFont('Segoe UI', 9))
+
         self.TableCitasActivas = QTableView(self.GpCitas)
         self.TableCitasActivas.setGeometry(16, 32, 633, 569)
         self.TableCitasActivas.setFont(QFont('Segoe UI', 9))
         self.TableCitasActivas.setModel(QStandardItemModel())
         self.TableCitasActivas.setAlternatingRowColors(True)
+
+        # Botones de accion de la aplicacion
         self.BtnDiagnostico = QToolButton(self.GpDiagnostico)
         self.BtnDiagnostico.setGeometry(16, 336, 160, 48)
         self.BtnDiagnostico.setFont(QFont('Segoe UI', 9))
         self.BtnDiagnostico.setText('Registrar Diagnostico')
         self.BtnDiagnostico.clicked.connect(self.BtnDiagnostico_clicked)
+
         self.BtnCancelarCita = QToolButton(self.GpDiagnostico)
         self.BtnCancelarCita.setGeometry(240, 336, 160, 48)
         self.BtnCancelarCita.setFont(QFont('Segoe UI', 9))
         self.BtnCancelarCita.setText('Cancelar Cita')
         self.BtnCancelarCita.clicked.connect(self.BtnCancelarCita_clicked)
+
         self.BtnReprogramar = QToolButton(self.GpDiagnostico)
         self.BtnReprogramar.setGeometry(456, 336, 160, 48)
         self.BtnReprogramar.setFont(QFont('Segoe UI', 9))
         self.BtnReprogramar.setText('Reprogramar Cita')
         self.BtnReprogramar.clicked.connect(self.BtnReprogramar_clicked)
+
         self.BtnCerrarSesion = QToolButton(self)
         self.BtnCerrarSesion.setGeometry(1184, 704, 160, 48)
         self.BtnCerrarSesion.setFont(QFont('Segoe UI', 9))
         self.BtnCerrarSesion.setText('Cerrar Sesion')
         self.BtnCerrarSesion.clicked.connect(self.BtnCerrarSesion_clicked)
-        self.lHistorialClinico = QLabel(self)
-        self.lHistorialClinico.setGeometry(928, 24, 208, 34)
-        self.lHistorialClinico.setFont(QFont('Segoe UI', 16, QFont.Weight.Bold))
-        self.lHistorialClinico.setText('Historial Clinico')
+
         self.BtnBuscarPAciente = QToolButton(self.GralData)
         self.BtnBuscarPAciente.setGeometry(480, 160, 120, 32)
         self.BtnBuscarPAciente.setFont(QFont('Segoe UI', 9))
         self.BtnBuscarPAciente.setText('Buscar Paciente')
         self.BtnBuscarPAciente.clicked.connect(self.BtnBuscarPAciente_clicked)
-        self.lineEdit1 = QLineEdit(self.GralData)
-        self.lineEdit1.setGeometry(120, 160, 345, 33)
-        self.lineEdit1.setFont(QFont('Segoe UI', 9))
+
+        # Labels de cambio segun el inicio de sesion 
         self.LblEspecialidadDoc = QLabel(self.GralData)
         self.LblEspecialidadDoc.setGeometry(144, 104, 304, 29)
         self.LblEspecialidadDoc.setFont(QFont('Segoe UI', 14, QFont.Weight.Bold))
